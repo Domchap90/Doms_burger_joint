fa5e6b9032df69417a813e4afc7bf2f921f14346
Domchap90 <domchaple@gmail.com>
2020-08-04 10:02:08 +0000

    Menu views and urls implemented. Menu template created with basic layout of items.

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Food_Item
 
 # Create your views here.
+
+
+def menu(request):
+    """ A view to reveal the menu items """
+
+    items = Food_Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'menu/menu_items.html', context)
