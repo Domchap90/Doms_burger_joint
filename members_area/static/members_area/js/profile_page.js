e4474375562106d6b270f67f243b1d4311f3965f
Domchap90 <domchaple@gmail.com>
2020-11-02 11:17:25 +0000

    Collapsible order history table allows only one row view at a time & ability to close the row to no views.

---
+++ members_area/static/members_area/js/profile_page.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('.detail-order-row').hide();
+    $('.general-order-row').click(function(){
+    // Creates accordian effect where only one row's details can be viewed at a time 
+        let detailRowId = $(this).next().attr('id');
+
+        // Allows the single viewed row to be closed so all rows are hidden
+        $('.detail-order-row:not(#'+detailRowId+')').hide();
+        $(this).next().toggle();
+    });
+    controlPaginationMenuLength();
+});
+
+// Change number of pagination buttons according to screen size
+window.addEventListener('resize', controlPaginationMenuLength);
+
+function controlPaginationMenuLength() {
+    let paginatedNumberBtns = $('.pagination li:not(.arrow-btn)');
+    paginatedNumberBtns.hide();
+    let activeElement = parseInt($('.pagination .active').html());
+    
+    if ($(window).width () < 601) {
+        // show 5 page buttons below 601px res
+        displayButtons(paginatedNumberBtns, activeElement, 5);
+    } else if ($(window).width () < 901) {
+        displayButtons(paginatedNumberBtns, activeElement, 7);
+    } else {
+        displayButtons(paginatedNumberBtns, activeElement, 10);
+    }
+}
+
+function displayButtons(paginatedNumberBtns, activeElement, numPageBtnsToDisplay) {
+    /* Limits the number of paginated buttons to be displayed when there are many */
+    
+    let btnsToDisplay = []
+    // limits will dictate the number of buttons either side of the active element
+    let limit = numPageBtnsToDisplay/2;
+
+    for (numberBtn of paginatedNumberBtns){
+        // read page number from id of button
+        let numberBtnValue = parseInt(numberBtn.id.split('_')[1]);
+        if (activeElement > limit) {
+        // enforces limits when active element is greater than number of pages to be displayed / 2
+            if(numberBtnValue <= (activeElement + limit) && numberBtnValue > (activeElement - limit)) {
+                btnsToDisplay.push(numberBtn);
+            }
+        } else {
+        // otherwise show the first number of pages to be displayed as per input
+            if(numberBtnValue < numPageBtnsToDisplay + 1) {
+                btnsToDisplay.push(numberBtn);
+            }
+        }
+    }
+    for (btn of btnsToDisplay) {
+        $(btn).show();
+    }
+}
